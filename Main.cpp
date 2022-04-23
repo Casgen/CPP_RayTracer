5e5e06d0167ece7165cbc77f96cee41986aaaba9
Joseff <vzotov42@gmail.com>
2022-04-23 22:15:34 +0200

    Added a Dielectric Material, Refactored Camera class just a tiny bit

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,8 @@
+#include <memory>
 #include <thread>
 #include <SFML/Graphics.hpp>
 #include "math/Color.h"
+#include "model/Dielectric.h"
 #include "model/Lambertian.h"
 #include "model/Metal.h"
 #include "model/Sphere.h"
@@ -13,7 +15,9 @@ int main()
 {
     sf::RenderWindow window(sf::VideoMode(width, height), "SFML works!");
 
-    Camera camera = Camera(static_cast<float>(width), static_cast<float>(height));
+    float focusDist = length(vec3(0.f) - vec3(1.f, 0.f, 0.f));
+    Camera camera = Camera(width, height);
+    
     Scene scene = Scene(camera);
 
     /**
@@ -23,16 +27,16 @@ int main()
      * (rvalue vs. lvalue)
      */
 
+    std::shared_ptr matDielectric = std::make_shared<Dielectric>(1.5f);
 
     Sphere leftSphere = Sphere(vec3(1.f, 0.3f, 0.f), Color(0.5f, 0.468f, 0.89f), 0.15f,
-                              std::make_shared<Metal>(Color(0.156f, 0.8f, 0.214f)));
-    
-    Sphere rightSphere = Sphere(vec3(1.f, -0.3f, 0.f), Color(0.5f, 0.468f, 0.89f), 0.15f,
-                              std::make_shared<Metal>(Color(0.487f, 0.214f, 0.874f),0.5f));
-    
+                               std::make_shared<Metal>(Color(0.156f, 0.8f, 0.214f)));
+
+    Sphere rightSphere = Sphere(vec3(1.f, -0.3f, 0.f), Color(0.5f, 0.468f, 0.89f), 0.15f, matDielectric);
+
     Sphere middleSphere = Sphere(vec3(1.f, 0.f, 0.f), Color(0.5f, 0.468f, 0.89f), 0.15f,
-                              std::make_shared<Lambertian>(Color(0.f, 0.7f, 0.7f)));
-    
+                                 std::make_shared<Lambertian>(Color(0.f, 0.7f, 0.7f)));
+
     Sphere bottomSphere = Sphere(vec3(1.f, 0.f, 3.2f), Color(0.5f, 0.468f, 0.89f), 3.f,
                                  std::make_shared<Lambertian>(Color(0.8f, 0.8f, 0.f)));
     scene.AddHittableObject(&leftSphere);
@@ -42,7 +46,7 @@ int main()
 
     sf::Image img = sf::Image();
     Renderer renderer = Renderer(scene, height, width, img);
-    
+
     std::thread worker = std::thread(&Renderer::Render, &renderer);
 
     while (window.isOpen())
---
+++ model/Dielectric.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include "HitRecord.h"
+#include "Material.h"
+#include "../math/MathUtils.h"
+
+class Dielectric : public Material
+{
+public:
+    float indexOfRefraction;
+
+    /**
+     *  Creates a dielectric material with an index of refraction
+     *  @param etai - an Index of refraction (
+     */
+    Dielectric(const float& etai) : indexOfRefraction(etai) {}
+
+    bool Scatter(const Ray& rayIn, const HitRecord& record, Color& attenuation, Ray& scattered) const override
+    {
+        attenuation = Color(1.f);
+        float refractionRatio = record.frontFace ? 1.0f / indexOfRefraction : indexOfRefraction;
+
+        vec3 unitDirection = normalize(rayIn.direction);
+        float cosTheta = fmin(dot(-unitDirection, record.normal), 1.0);
+        float sinTheta = sqrt(1.0f - cosTheta*cosTheta);
+
+        bool cannotRefract = refractionRatio * sinTheta > 1.0f;
+        vec3 direction;
+
+        if (cannotRefract || reflectance(cosTheta, refractionRatio) > MathUtils::RandomFloat())
+            direction = MathUtils::Reflect(unitDirection, record.normal);
+        else
+            direction = MathUtils::Refract(unitDirection, record.normal, refractionRatio);
+
+        scattered = Ray(record.point, direction);
+        return true;
+    }
+
+    private:
+        float reflectance(float cos, float refIdx) const
+        {
+            float r0 = (1-refIdx)/(1+refIdx);
+            r0 = r0*r0;
+            return r0 + (1.f-r0)*pow(1-cos,5);
+        }
+};
+
